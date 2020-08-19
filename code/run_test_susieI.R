d299f605c048cb4c38742703b239c24ae6575591
simingz <michellezhao.student@gmail.com>
2020-08-19 15:54:56 -0500

    susieI

--- code/run_test_susieI.R
+++ code/run_test_susieI.R
@@ -17,13 +17,13 @@ codedir <- "/project2/mstephens/causalTWAS/causal-TWAS/code/"
 source(paste0(codedir, "stats_func.R"))
 source(paste0(codedir,"input_reformat.R"))
 
-
 addHandler(writeToFile, file="run_test_susie.R.log", level='DEBUG')
 loginfo('script started ... ')
 
-PIPfilter <- 0.02
+PIPfilter <- 0.3
 chunksize = 500000
 Niter <- 10
+ifshuffle <- T
 
 loginfo("regional PIP cut: %s ", PIPfilter)
 loginfo("region size: %s", chunksize)
@@ -44,29 +44,38 @@ load(args[3])
 phenores$Y <-  phenores$Y - mean(phenores$Y)
 
 
+# gres <- paste0(args[4], ".expr.txt")
+# pipres <- read.table(gres, header =T)
+#
+# regions <- NULL
+# chroms <- unique(pipres$chr)
+# for (chrom in chroms){
+#   pipres.chr <- pipres[pipres$chr == chrom, ]
+#   stpos <- min(pipres.chr$p0) - chunksize/2
+#   edpos <- max(pipres.chr$p1) + chunksize/2
+#   p0 <- stpos + 0: floor((edpos - stpos)/chunksize) * chunksize
+#   p1 <- stpos + 1: ceiling((edpos - stpos)/chunksize) * chunksize
+#   itv <- cbind(p0, p1)
+#   rPIP <- apply(itv, 1, function(x) sum(pipres.chr[x[1] < pipres.chr$p0 & pipres.chr$p0 < x[2], "PIP"]))
+#   nCausal <- apply(itv, 1, function(x) sum(pipres.chr[x[1] < pipres.chr$p0 & pipres.chr$p0 < x[2], "ifcausal"]))
+#   itv <- cbind(chrom, itv, rPIP, nCausal)
+#   regions <- rbind(regions, itv)
+# }
+#
+# loginfo("No. intervals: %s", nrow(regions))
+# write.table(regions , file = paste0(args[4], ".rPIP.txt")  , row.names=F, col.names=T, sep="\t", quote = F)
+
+regions <- read.table(paste0(args[4], ".rPIP.txt"), header = T)
+print(nrow(regions))
+
 gres <- paste0(args[4], ".expr.txt")
-pipres <- read.table(gres, header =T)
+sres <- paste0(args[4], ".snp.txt")
+pipres <- rbind(read.table(gres, header =T), read.table(sres, header =T))
+cau <- pipres[pipres$ifcausal == 1, "name"]
 
-regions <- NULL
-chroms <- unique(pipres$chr)
-for (chrom in chroms){
-  pipres.chr <- pipres[pipres$chr == chrom, ]
-  stpos <- min(pipres.chr$p0) - chunksize/2
-  edpos <- max(pipres.chr$p1) + chunksize/2
-  p0 <- stpos + 0: floor((edpos - stpos)/chunksize) * chunksize
-  p1 <- stpos + 1: ceiling((edpos - stpos)/chunksize) * chunksize
-  itv <- cbind(p0, p1)
-  rPIP <- apply(itv, 1, function(x) sum(pipres.chr[x[1] < pipres.chr$p0 & pipres.chr$p0 < x[2], "PIP"]))
-  nCausal <- apply(itv, 1, function(x) sum(pipres.chr[x[1] < pipres.chr$p0 & pipres.chr$p0 < x[2], "ifcausal"]))
-  itv <- cbind(chrom, itv, rPIP, nCausal)
-  regions <- rbind(regions, itv)
-}
 
-loginfo("No. intervals: %s", nrow(regions))
-write.table(regions , file = paste0(args[4], ".rPIP.txt")  , row.names=F, col.names=T, sep="\t", quote = F)
-
-regions <- regions[regions[, "rPIP"] > PIPfilter,  ]
-loginfo("No. intervals for after PIP filter: %s", nrow(regions))
+regions <- regions[regions[, "rPIP"] > PIPfilter & regions[, "nCausal"] > 0,  ] # select regions
+loginfo("No. intervals after PIP filter: %s", nrow(regions))
 
 #TODO: join regions based on LD genes, add a column indicating which region should be linked
 regions <- as.data.frame(regions)
@@ -85,27 +94,66 @@ for (rn in unique(regions[, "rname"])){
 
     name <- paste(chr, p0, p1, sep = "-")
 
-    idx.gene <- exprres$chrom == chr & exprres$p0 > p0 & exprres$p1 < p1
+    idx.gene <- exprres$chrom == chr & exprres$p0 > p0 & exprres$p0 < p1
     idx.SNP <- dat$chr == chr & dat$pos > p0 & dat$pos < p1
 
     X.gene <- exprres$expr[ , idx.gene, drop = F]
     X.SNP <-  dat$G[ ,idx.SNP]
     X <- cbind(X.gene, X.SNP)
 
-    anno.gene <- cbind(colnames(X.gene), exprres$chrom[idx.gene],  exprres$p0[idx.gene], "gene")
+    anno.gene <- cbind(colnames(X.gene), exprres$chrom[idx.gene],  exprres$p0[idx.gene],
+                       rep("gene", length(idx.gene[idx.gene])))
     anno.SNP <- cbind(dat$snp[idx.SNP,], dat$chr[idx.SNP,], dat$pos[idx.SNP,], "SNP")
     anno <- rbind(anno.gene, anno.SNP)
     colnames(anno) <-  c("name", "chr", "pos", "type")
+    anno <- as.data.frame(anno)
+    anno$ifcausal <- ifelse(anno$name %in% cau, 1, 0)
+
     regionlist[[rn]][["X"]] <- cbind(regionlist[[rn]][["X"]], X)
     regionlist[[rn]][["anno"]] <- rbind(regionlist[[rn]][["anno"]], anno)
   }
 }
 
-rm(dat);gc()
-save.image(file = "temp.Rd")
+
+for (rn in names(regionlist)){
+  anno <- regionlist[[rn]][["anno"]]
+  if (nrow(anno[anno$type == "gene",]) == 0) {
+    regionlist[[rn]] <- NULL
+  }
+}
+loginfo("limiting to regions with at least one gene: %s", length(regionlist))
+
+
+if (isTRUE(ifshuffle)){
+
+  loginfo("shuffling genes to different regions")
+
+  regionlist.sh <- list()
+  rn.sh <- sample(names(regionlist))
+
+  for (i in 1:length(regionlist)){
+    rn.snp <- names(regionlist)[i]
+    rn.gene <- rn.sh[i]
+    anno.snp <- regionlist[[rn.snp]][["anno"]]
+    anno.gene <- regionlist[[rn.gene]][["anno"]]
+    rn <- rn.snp
+    regionlist.sh[[rn]][['X']] <- cbind(regionlist[[rn.gene]][['X']][, anno.gene$type == "gene"],
+                                        regionlist[[rn.snp]][['X']][, anno.snp$type == "SNP"])
+
+    regionlist.sh[[rn]][['anno']] <- rbind(anno.gene[anno.gene$type == "gene", ],
+                                           anno.snp[anno.snp$type == "SNP", ])
+  }
+  regionlist <- regionlist.sh
+}
+
 
 outname <- args[5]
 loginfo("susie started for %s", outname)
+
+rm(dat); gc()
+save.image(file = "temp.Rd")
+saveRDS(regionlist, file = paste0(outname, "_regionlist.rds"))
+
 
 prior.SNP <- NULL
 prior.gene <- NULL
@@ -130,8 +178,9 @@ for (iter in 1:Niter){
     gene.rpiplist[[rn]] <- sum(susieres[[rn]]$alpha[regionlist[[rn]][["anno"]][,"type"] == "gene"])
     outdf <- rbind(outdf, cbind(regionlist[[rn]]$anno, t(susieres[[rn]]$alpha), susieres[[rn]]$pip))
   }
-
-  write.table( outdf , file= paste0(outname, ".", iter, ".txt" ) , row.names=F, col.names=T, sep="\t", quote = F)
+  colnames(outdf)[6:7] <- c("susie_alpha", "susie_pip")
+  write.table(outdf , file= paste0(outname, ".", iter, ".txt" ) , row.names=F, col.names=T, sep="\t", quote = F)
+  save( snp.rpiplist, gene.rpiplist, file = paste(outname, iter, "susieIres.Rd", sep = "."))
   prior.SNP <- mean(unlist(snp.rpiplist))
   prior.gene <- mean(unlist(gene.rpiplist))
   print(c(prior.SNP, prior.gene))
