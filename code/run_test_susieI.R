41a7f93ddb5a8f4d12a06efa26be4940a5b20f19
simingz <michellezhao.student@gmail.com>
2020-11-24 13:05:02 -0600

    credible set

--- code/run_test_susieI.R
+++ code/run_test_susieI.R
@@ -20,6 +20,7 @@ codedir <- "/project2/mstephens/causalTWAS/causal-TWAS/code/"
 source(paste0(codedir, "stats_func.R"))
 source(paste0(codedir,"input_reformat.R"))
 source(paste0(codedir,"susie_filter.R"))
+source(paste0(codedir,"susieI_func.R"))
 
 addHandler(writeToFile, file="run_test_susie.R.log", level='DEBUG')
 loginfo('script started ... ')
@@ -48,6 +49,11 @@ prior.gene_init <- NULL
 prior.SNP_init <- NULL
 L <- 1
 plugin_prior_variance <- F # using truth when T for now
+coverage <- 0.95 # credible set coverage
+
+Niter2 <- 20
+filterandrerun <- F
+P2cut <- 0.5
 
 Ncore <- 1
 
@@ -154,114 +160,58 @@ for (b in 1:length(pfiles)){
 
 write.table(regionsall, file= paste0(outname,".", filtertype, ".r.txt" ) , row.names=F, col.names=T, sep="\t", quote = F)
 
-stop()
-
 # start susieI
 loginfo("susie started for %s", outname)
 
-
-cl <- makeCluster(Ncore,outfile="")
-registerDoParallel(cl)
-
-
-varY <- var(phenores$Y)
 V.g <- phenores$batch[[1]]$param$sigma_beta ** 2 # prior variance for gene
 V.s <- phenores$batch[[1]]$param$sigma_theta ** 2 # prior variance for SNP
 
-prior.SNP_rec <- rep(0, Niter)
-prior.gene_rec <- rep(0, Niter)
+pars <- susieI(prior.gene_init = prior.gene_init,
+                   prior.SNP_init = prior.SNP_init,
+                   regionlist = regionlist,
+                   outname = outname,
+                   iter = Niter,
+                   Ncore = Ncore)
 
-for (iter in 1:Niter){
 
-  loginfo("run iteration %s", iter)
 
-  snp.rpiplist <- list()
-  gene.rpiplist <- list()
-  outdf <- foreach (b = 1:22, .combine = "rbind",.packages = c("susieR", "bigstatsr")) %dopar% {
-    source(paste0(codedir, "susie_func.R"))
+if (isTRUE(filterandrerun)){
 
-    # load genotype data
-    load(pfileRds[b])
+  prior.gene_init2 <- pars[["prior.gene"]]
+  prior.SNP_init2 <- pars[["prior.SNP"]]
 
-    # load expression Rd file, variable: exprres
-    load(efiles[b])
+  regionlist2 <- regionlist
+  P2list <- list()
 
-    cau <- c(dat$snp[phenores$batch[[b]]$param$idx.cSNP,],
-             exprres$gnames[phenores$batch[[b]]$param$idx.cgene])
-
-    # run susie for each region (in parallel by batch)
-    outdf.b.list <- list()
+  for (b in 1:length(pfiles)){
+    P2list[[b]] <- list()
     for (rn in names(regionlist[[b]])) {
-      print(c(iter, b, rn))
 
       gidx <- regionlist[[b]][[rn]][["gidx"]]
       sidx <- regionlist[[b]][[rn]][["sidx"]]
-      p <- length(gidx[gidx]) + length(sidx[sidx])
-      prior <- rep(0, p)
+      p.g <- length(gidx[gidx])
+      p.s <- length(sidx[sidx])
+      P2 <- 1 - (1- prior.gene_init2)**p.g * (1- prior.SNP_init2)**p.s
+        - p.g * prior.gene_init2 * (1 - prior.gene_init2) ** (p.g - 1) * (1- prior.SNP_init2) ** p.s
+        - p.s * (1 - prior.gene_init2) ** p.g * prior.SNP_init2 * (1- prior.SNP_init2) ** (p.s - 1)
 
-      if (is.null(prior.gene_init) | is.null(prior.SNP_init)){
-        prior.gene_init <- 1/p
-        prior.SNP_init <- 1/p
+      P2 <- p.g * prior.gene_init2 * (1 - prior.gene_init2) ** (p.g - 1) * (1- prior.SNP_init2) ** p.s
+      + p.s * (1 - prior.gene_init2) ** p.g * prior.SNP_init2 * (1- prior.SNP_init2) ** (p.s - 1)
+
+
+      P2list[[b]][[rn]] <- P2
+      if (P2 >= P2cut){
+        regionlist2[[b]][[rn]] <- NULL
       }
 
-      if (iter == 1) {
-        prior <- c(rep(prior.gene_init, length(gidx[gidx])), rep(prior.SNP_init, length(sidx[sidx])))
-      } else {
-        prior <- c(rep(prior.gene, length(gidx[gidx])), rep(prior.SNP, length(sidx[sidx])))
-      }
-
-      if (isTRUE(plugin_prior_variance)){
-        V.scaled <- c(rep(V.g/varY, length(gidx[gidx])), rep(V.s/varY, length(sidx[sidx])))
-        V.scaled <- matrix(rep(V.scaled, each = L), nrow=L)
-      } else{
-        V.scaled <- matrix(rep(0.2, L * p), nrow = L) # following the default in susieR
-      }
-
-      if (isTRUE(ifnullweight)){
-        nw <- max(0, 1 - sum(prior))
-        prior <- prior/(1-nw)
-      } else {
-        nw <- NULL
-      }
-
-      X <- cbind(exprres$expr[, gidx], dat$G[, sidx])
-
-      susieres <- susie(X, phenores$Y, L = L, prior_weights = prior,
-                        null_weight = nw, scaled_prior_variance = V.scaled,
-                        estimate_prior_variance = !plugin_prior_variance)
-
-      anno.gene <- cbind(exprres$gnames[gidx], exprres$chrom[gidx],  exprres$p0[gidx],
-                         rep("gene", length(gidx[gidx])))
-      anno.SNP <- cbind(dat$snp[sidx,], dat$chr[sidx,], dat$pos[sidx,],
-                        rep("SNP", length(sidx[sidx])))
-
-      anno <- rbind(anno.gene, anno.SNP)
-      colnames(anno) <-  c("name", "chr", "pos", "type")
-      anno <- as.data.frame(anno)
-      anno$ifcausal <- ifelse(anno$name %in% cau, 1, 0)
-
-      outdf.rn <- cbind(anno, susieres$pip)
-      colnames(outdf.rn)[6] <- "susie_pip"
-
-      outdf.b.list[[rn]] <- outdf.rn
     }
-
-    outdf.b <- do.call(rbind, outdf.b.list)
-    outdf.b
   }
 
-  prior.SNP <- mean(outdf[outdf[ , "type"] == "SNP", "susie_pip"])
-  prior.gene <- mean(outdf[outdf[ , "type"] == "gene", "susie_pip"])
-  print(c(prior.SNP, prior.gene))
 
-  prior.SNP_rec[iter] <- prior.SNP
-  prior.gene_rec[iter] <- prior.gene
-  save(prior.gene_rec, prior.SNP_rec, file = paste(outname, "susieIres.Rd", sep = "."))
-  gc()
-  write.table(outdf, file= paste0(outname, ".susieI.txt" ) , row.names=F, col.names=T, sep="\t", quote = F)
 }
 
-stopCluster(cl)
+
+
 
 loginfo("susie done for %s", outname)
 
